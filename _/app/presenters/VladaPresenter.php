bab68137987a16d02350a6f93fc60da1d53c4b04
LS <l.sevcuj@gmail.com>
2013-02-15 02:42:48 +0100

    horní navigace - menu (rok, vlada) + filtr (osoba,ministr)
    a dalsi drobne upravy css a js

--- _/app/presenters/VladaPresenter.php
+++ _/app/presenters/VladaPresenter.php
@@ -9,8 +9,58 @@ class VladaPresenter extends BasePresenter
 	public function renderDefault()
 	{
 		$osa = $this->getService('timeline');
-		$osaData = $this->getService('casovaOsa2')->where('start_parent_id IS NOT NULL OR konec_parent_id IS NOT NULL');
-		$this->template->osa = $osa->order('datum ASC, typ DESC');
+
+		$kabinet = $this->getService('kabinet');
+		$osoby = $this->getService('osoby');
+
+		$rokyData = $this->getService('casovaOsa2');
+		$roky = array();
+		$vlady = array();
+		foreach ($rokyData as $rok) {
+			// priprava menu roky
+			if (is_object($rok->start_datum)) {
+				$r = $rok->start_datum->format('Y');
+				if ($r > 0) {
+					$roky[$r] = $r;
+				}
+			}
+			if (is_object($rok->konec_datum)) {
+				$r = $rok->konec_datum->format('Y');
+				if ($r > 0) {
+					$roky[$r] = $r;
+				}
+			}
+			//priprava menu vlada
+			if ($rok->kabinet_id == 2) {
+				$r = $rok->start_datum->format('Y');
+				$vlady[$rok->start_datum->format('Y-m-d').'#'.$rok->id] = $r . " " .$rok->osoba->cele_jmeno ;
+			}
+		}
+
+		ksort($roky);
+		ksort($vlady);
+
+		// uprava aby v klici byl jen id na ktere budu odkazovat pomoci #
+		$vladyTmp = array();
+		foreach($vlady as $key => $value) {
+			preg_match('/#(\d+)/i', $key, $match);
+			$vladyTmp[$match[1]] = $value;
+		}
+		$vlady = $vladyTmp;
+
+		// casova osa cela nebo je to filtrace
+		$osaData = $this->getService('casovaOsa2');
+		if (!empty($_GET['osoba'])) {
+			$osa->where('osoba_id', $_GET['osoba']);
+			$filtrOsoba = clone $osoby;
+			$this->template->filtrOsoba = $filtrOsoba->get($_GET['osoba']);
+		} elseif (!empty($_GET['kabinet'])) {
+			$osa->where('kabinet_id', $_GET['kabinet']);
+			$filtrKabinet = clone $kabinet;
+			$this->template->filtrKabinet = $filtrKabinet->get($_GET['kabinet']);
+		} else {
+			$osa->where('parent_id = 0 OR parent_id IS NULL');
+		}
 
 		// preskladat podslozky, usetrim sql
 		$podrizeni = array();
@@ -64,8 +114,13 @@ class VladaPresenter extends BasePresenter
 
 		}
 
+		$this->template->osa = $osa->order('datum ASC, typ DESC');
 		$this->template->data = $osaData;
 		$this->template->data2 = $podrizeni;
+		$this->template->roky = $roky;
+		$this->template->vlady = $vlady;
+		$this->template->osoby = $osoby->order('prijmeni ASC, jmeno ASC');
+		$this->template->kabinet = $kabinet->order('nazev ASC');
 	}
 
 }
